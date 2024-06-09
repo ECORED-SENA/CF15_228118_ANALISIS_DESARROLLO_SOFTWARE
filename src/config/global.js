e0b8e5fc2f1f2f64e9f701a1513604521bee7e8e
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-06-09 01:54:21 -0500

    avances

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,9 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
+    componenteFormativo: 'Arquitectura aplicaciones móviles nativas',
+    descripcionCurso:
+      'En este componente formativo se abordan los conceptos básicos de las aplicaciones móviles nativas, los cuales servirán para identificar los tipos de metodologías usadas en el desarrollo móvil, en plataformas nativas, en entornos de desarrollo y su configuración.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
@@ -12,6 +13,10 @@ export default {
       {
         clases: ['banner-principal-decorativo-2'],
         imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.svg'),
       },
     ],
   },
@@ -31,27 +36,49 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo:
+          'Enfoque metodológico para el desarrollo de aplicaciones móviles nativas',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
-            hash: 't_1_1',
-          },
-        ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Plataformas de desarrollo móvil nativas',
         desarrolloContenidos: true,
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Entornos de desarrollo',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Plataforma Android',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Android Studio',
+            hash: 't_3_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Arquitectura de una aplicación en Android',
+            hash: 't_3_3',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Instalación y configuración entorno Android Studio',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo: 'Componentes de una aplicación Android',
         desarrolloContenidos: true,
       },
     ],
@@ -102,31 +129,121 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Metodología XP',
+      referencia:
+        'Perú, M. H. (2019, 15 de diciembre). ¿Qué es la programación extrema? Metodología Ágil XP (Ciclo de Vida XP, Prácticas Básicas de XP) [Video]. YouTube.',
+      tipo: 'Video YouTube',
+      link: 'https://www.youtube.com/watch?v=tCl33R9jHBk',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Metodología Scrum',
+      referencia:
+        'Henao, C. (2018, 27 de junio). #3. SCRUM en 6 minutos Metodologías Ágiles [Video]. YouTube.',
+      tipo: 'Video YouTube',
+      link: 'https://www.youtube.com/watch?v=HhC75IonpOU',
+    },
+    {
+      tema: 'Mobile-D',
+      referencia:
+        'Montoya, J. L. (2021, 15 de abril). Exposición metodologías de desarrollo de software.',
+      tipo: 'Mobile-D',
+      link: 'https://www.youtube.com/watch?v=I3yMHHgQCEk',
+    },
+    {
+      tema: 'Entornos de Desarrollo',
+      referencia:
+        'Peñalba, I. (2021). Hacer aplicaciones para Android: ¿Qué es un entorno de desarrollo y cuál es el mejor? El Español.',
+      tipo: 'Página',
+      link:
+        'https://www.elespanol.com/elandroidelibre/20200518/hacer-aplicaciones-android-entorno-desarrollo-mejor/490952339_0.html',
+    },
+    {
+      tema: 'Visual Studio',
+      referencia:
+        'Microsoft Build. (2023). Información general sobre Visual Studio.',
+      tipo: 'Página',
+      link:
+        'https://docs.microsoft.com/es-es/visualstudio/get-started/visual-studio-ide?view=vs-2019',
+    },
+    {
+      tema: 'Android',
+      referencia:
+        'Developers. (2020, 7 de mayo). Arquitectura de la plataforma.',
+      tipo: 'Página Oficial',
+      link: 'https://developer.android.com/guide/platform?hl=es-419',
+    },
+    {
+      tema: 'Versiones Android',
+      referencia: 'ADSLZone. (2021, 3 de marzo). Qué es Android.',
+      tipo: 'Página',
+      link: 'https://www.adslzone.net/reportajes/software/que-es-android',
+    },
+    {
+      tema: 'Versiones Android Studio',
+      referencia:
+        'Developer. (2021, 14 de junio). Archivos de descarga de Android Studio.',
+      tipo: 'Página',
+      link: 'https://developer.android.com/studio/archive?hl=es-419',
+    },
+    {
+      tema: 'Instalación Android',
+      referencia:
+        'Developer. (2020, 21 de diciembre). Cómo instalar Android Studio.',
+      tipo: 'Página Oficial',
+      link: 'https://developer.android.com/studio/install?hl=es-419',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Dalvik',
+      significado: 'máquina virtual de Android.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Manifiesto ágil',
+      significado:
+        'es un documento redactado con principios para mejorar la forma de desarrollar.',
+    },
+    {
+      termino: 'Wereable',
+      significado:
+        'dispositivo conectado que se puede llevar puesto y que se conecta al teléfono móvil.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Abrahamsson, P., Hanhineva, A., Hulkko, H., Ihme, T., Jäälinoja, J., Korkala, M., & Salo, O. (2004). Mobile-D: An agile approach for mobile application development. En Companion to the 19th Annual ACM SIGPLAN Conference on Object-Oriented Programming Systems, Languages, and Applications, 174-175. ACM.',
+      link: '',
+    },
+    {
+      referencia:
+        'Astel, D. (2003). Test-driven development: A practical guide. Prentice Hall PTR.',
+      link: '',
+    },
+    {
+      referencia:
+        'Avison, D. E., & Fitzgerald, G. (2006). Information system development. McGraw-Hill Education.',
+      link: '',
+    },
+    {
+      referencia:
+        'Balafuera, Y. D. A. (2015). Metodologías ágiles en el desarrollo de aplicaciones para dispositivos móviles: Estado actual. Revista de tecnología, 12(2).',
+      link: 'https://doi.org/10.18270/rt.v12i2.1291',
+    },
+    {
+      referencia:
+        'Letelier, P., Canós, J. H., & Penadés, M. C. (2003). Metodologías ágiles en el desarrollo de software. Presentado en VIII Jornadas de Ingeniería del Software y Bases de Datos (JISBD), Alicante, España, 1-8. Addison Wesley.',
+      link: '',
+    },
+    {
+      referencia:
+        'Olmos, F. (2020, 19 de octubre). El origen de Android: Características y costos. Fredy Olmos.',
+      link:
+        'https://fredyolmos.com/ciencia-y-tecnologia/el-origen-de-android-costos-caracteristicas/',
+    },
+    {
+      referencia:
+        'Takeuchi, H., & Nonaka, I. (1986). The new new product development game. Harvard Business Review.',
       link: '',
     },
   ],
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,6 +9,41 @@
           i.fas.fa-info
       h1 Introducción
     
+    .row.align-items-center.mb-4
+      .col-md-2
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/1.svg",style="max-width: 123px;")
+      .col-md-10
+        p.mb-0 Sin duda, los dispositivos móviles son parte de nuestro día a día y cada vez son más avanzados. La creciente demanda de #[em software] específico para estos dispositivos ha generado nuevos desafíos para los desarrolladores, ya que este tipo de aplicaciones posee características propias, restricciones y necesidades únicas.
+    
+    .fondo-personalizado-1.pos-1.mb-4
+      .fondo-personalizado-1__contenido.pb-5
+        .row.mb-4.align-items-center
+          .col-lg-8
+            .fondo-color-sistema-i.p-4.borde-10.mb-4
+              p.mb-0 Es importante definir cuáles serán las tecnologías más adecuadas para la programación de móviles, y una de las arquitecturas más implantadas es la proporcionada por el sistema Android. En los dispositivos móviles actuales pueden utilizarse tres tipos de aplicaciones diferentes: nativas, #[b híbridas y web]. En este componente se tratarán las aplicaciones móviles nativas.
+            .fondo-color-primario.p-4.borde-10
+              p.mb-0.text-white Las aplicaciones nativas son aquellas que enfocan su desarrollo en una plataforma específica como #[b Android, iOS y Windows], haciendo uso de su correspondiente lenguaje de programación, el cual permite tener compatibilidad con sus características. En el caso de Android, se utilizan lenguajes como Java y Kotlin; para la plataforma iOS, Objective-C o Swift; y para la plataforma Windows, los lenguajes C++ o C#.
+          .col-lg-4.order-first.order-lg-last
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/2.svg",style="max-width: 400px;")
+        
+        .fondo-color-sistema-g.p-4.borde-10
+          .row.align-items-center 
+            .col-lg-2
+              figure.mb-4.mb-lg-0
+                img.mx-auto(src="@/assets/curso/temas/3.png",style="max-width: 327px;")
+            .col-lg-10
+              p.mb-0 Las aplicaciones nativas son las más eficientes y pueden salir a producción en las tiendas más populares como #[b App Store, Google Play y AppGallery], entre otras. Una de sus características principales es que permiten enviar notificaciones para realizar promociones comerciales de negocios, productos, etc. En cuanto al tiempo de desarrollo, este es mucho más largo y, por tanto, son más costosas debido a la especialización que deben tener los programadores en cada lenguaje. Su uso se encuentra en aplicaciones complejas como editores de video, aplicaciones de medicina, aplicaciones de ingeniería,  entre otros.
+    
+    .row 
+      .col-auto.d-none.d-md-block 
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/5.svg",style="max-width: 85px;")
+      .col 
+        p.mb-0 Desde el punto de vista de los programadores, uno de los mayores inconvenientes es que para que una misma aplicación pueda utilizarse en dos o más plataformas distintas, hay que desarrollar la misma aplicación en los diferentes lenguajes, iniciando desde cero y necesitando más tiempo de desarrollo y mayor costo.
+
+    
 </template>
 
 <script>
