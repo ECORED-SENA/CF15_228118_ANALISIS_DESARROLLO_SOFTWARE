62c3e7b05f4085a2bcd99848a3a804dc5443883c
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-06-10 03:02:39 -0500

    avances

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,8 +5,280 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
-      
+      h1 Enfoque metodológico para el desarrollo de aplicaciones móviles nativas
+    
+    .fondo-personalizado-3.mb-5
+      .fondo-personalizado-3__contenido.pb-5
+        .row.align-items-end.mb-5
+          .col-lg-8
+            p.mb-0.mb-4 Actualmente, las metodologías ágiles tienen una gran notoriedad, ya que ofrecen una buena solución para desarrollar proyectos en tiempos más cortos, especialmente aquellos proyectos que constantemente experimentan cambios en sus requisitos. Las aplicaciones para dispositivos móviles son un buen ejemplo de esto, ya que requieren una variedad de características como #[b portabilidad, movilidad y adaptación], entre otras. Aunque hay muchas aplicaciones para dispositivos móviles que funcionan en diferentes plataformas, no siempre cumplen totalmente las expectativas de los usuarios debido a su baja calidad en el desarrollo. Esto se debe a que el uso de metodologías no es una prioridad en este tipo de aplicaciones, ignorando los métodos de ingeniería que son los que garantizan el mantenimiento y calidad de las aplicaciones.
+
+            .fondo-personalizado-2.p-4.borde-5
+              .row.align-items-center
+                .col-md-auto 
+                  .d-flex.mb-4.mb-md-0
+                    .burbuja-1.fondo-color-acento-contenido.mx-auto
+                      figure.p-1
+                        img.mx-auto(src="@/assets/curso/temas/6.svg",style="max-width: 72px;")
+                .col 
+                  p.color-acento-contenido.mb-4.fw-bold Según con Letelier y Canos (2003)
+                  p.mb-0.text-white Las metodologías ágiles son procesos para desarrollar #[em software] de manera rápida y con gran facilidad de adopción por los equipos de trabajo. Se han seleccionado las metodologías ágiles más referenciadas para emplearlas en el desarrollo de aplicaciones móviles, lo cual resulta muy conveniente teniendo en cuenta el crecimiento de los proyectos para estas tecnologías.
+          .col-lg-4.order-first.order-lg-last
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/7.svg",style="max-width: 400px;")
+        
+        .row.mb-5
+          .col-auto.d-none.d-md-block
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/8.svg",style="max-width: 85px;")
+          .col
+            p.mb-0 A continuación, se describen los conceptos principales para la introducción al uso de metodologías de desarrollo:
+
+        .row.justify-content-center.mb-3
+          .col-lg-5.mb-4(data-aos="fade-right")
+            .tarjeta-personalizada-1.p-5.h-100.pb-4
+              .burbuja-1.fondo-color-acento-contenido.me-auto.mb-2
+                figure.p-1
+                  img.mx-auto(src="@/assets/curso/temas/9.svg",style="max-width: 66px;")
+              br
+              h3.mb-4.fondo-color-acento-contenido.d-inline-block.px-2.pt-1 La metodología según Avison y Fitzgerald (2006):
+              p.mb-0.text-white Contribuye en la implementación de nuevos sistemas de información utilizando un conjunto de métodos, herramientas y documentos auxiliares. Una metodología se divide en fases y subfases para planificar, gestionar y controlar el proyecto. También permite seleccionar las técnicas más indicadas en cada proceso de un proyecto.
+          .col-lg-5.mb-4(data-aos="fade-left")
+            .tarjeta-personalizada-1.p-5.h-100.pb-4
+              .burbuja-1.fondo-color-acento-contenido.me-auto.mb-2
+                figure.p-1
+                  img.mx-auto(src="@/assets/curso/temas/10.svg",style="max-width: 69px;")
+              br
+              h3.mb-4.fondo-color-acento-contenido.d-inline-block.px-2.pt-1 La metodología ágil según Amaya Balaguera (2015):
+              p.mb-0.text-white Estas metodologías surgen como una alternativa a las metodologías tradicionales. Su enfoque se basa en los principios del manifiesto ágil, que propone un desarrollo iterativo. Su objetivo principal se centra en realizar una mejor captura de los requisitos cambiantes, la gestión de riesgos y la reducción del tiempo de desarrollo.
+        
+        .row
+          .col-auto.d-none.d-md-block
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/8.svg",style="max-width: 85px;")
+          .col
+            p.mb-0 Entre las principales metodologías ágiles aplicadas en el desarrollo de aplicaciones móviles, se destacan las siguientes:
+
+        .cont-titulo-decorativo.p-2.mt-3.mb-5
+          .row
+            .col-auto.pe-0
+              figure
+                img(src="@/assets/curso/temas/img-titulo-dec.svg")
+            .col.align-self-end
+              h3.mt-3.mb-0.me-2 Extreme Programming (XP)
+        
+        .row 
+          .col-lg-7
+            p.mb-0 Es una metodología liviana, aplicable en grupos de desarrollo pequeños y medianos que se centra en requerimientos no exactos y en continuo cambio. Se caracteriza por tener un ciclo de vida dinámico, a través de ciclos de desarrollo cortos denominados iteraciones. Sus fases son análisis, diseño, desarrollo y pruebas, en las cuales se aplican 12 prácticas.
+          .col-lg-5.order-first.order-lg-last
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/11.png",style="max-width: 505px;")
+
+    .fondo-personalizado-4.mb-5
+      .fondo-personalizado-4__contenido.pb-5
+        .row.justify-content-center
+          .col-lg-10
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 1.] Prácticas de XP
+            figure
+              img.mx-auto.d-none.d-lg-block(src="@/assets/curso/temas/13.svg",style="max-width: 1030px;",alt="Se presentan las fases planificación, diseño, codificación, pruebas y lanzamiento de prácticas XP.")
+              img.mx-auto.d-lg-none(src="@/assets/curso/temas/14.svg",style="max-width: 458px;",alt="Se presentan las fases planificación, diseño, codificación, pruebas y lanzamiento de prácticas XP.")
+
+    .row
+      .col-auto.d-none.d-md-block
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/16.svg",style="max-width: 85px;")
+      .col
+        p.mb-0 Así pues, XP no es solo un conjunto de técnicas, sino un conjunto de principios probados y fiables con características de las metodologías convencionales, pero llevadas a un nivel extremo.
+    
+    .cont-titulo-decorativo.p-2.mt-4.mb-5
+      .row
+        .col-auto.pe-0
+          figure
+            img(src="@/assets/curso/temas/img-titulo-dec.svg")
+        .col.align-self-end
+          h3.mt-3.mb-0.me-2 #[em SCRUM]
+    
+    p.mb-4 #[em Scrum], según Takeuchi & Nonaka (1986), presenta un proceso adaptativo, rápido y auto-organizado para el desarrollo de productos, centrado en la gestión de proyectos en situaciones que son difíciles de planificar a futuro. El término "Scrum" proviene del famoso juego de rugby y hace referencia a la forma en que se reintroduce un balón que ha salido del campo al terreno de juego de manera colectiva.
+    p.mb-4 Este modelo se basa en el desarrollo incremental, es decir, conforme avanzan las fases y la iteración, mayor es el tamaño del proyecto que se está desarrollando. Los procesos que utiliza son:
+
+    .row.align-items-end.mb-3
+      .col-lg-6
+        .row 
+          .col-lg-4.col-md-6.mb-4
+            .fondo-color-primario.borde-20.p-4.pb-5.text-center.h-100
+              i.fas.fa-cube.color-acento-contenido.mb-3(style="font-size: 28px;")
+              h4.mb-0.text-white #[em Product Backlog]
+          .col-lg-4.col-md-6.mb-4
+            .fondo-color-sistema-k.borde-20.p-4.pb-5.text-center.h-100
+              i.fas.fa-cube.color-acento-contenido.mb-3(style="font-size: 28px;")
+              h4.mb-0.text-white #[em Sprint Backlog]
+          .col-lg-4.col-md-6.mb-4
+            .fondo-color-sistema-k.borde-20.p-4.pb-5.text-center.h-100
+              i.fas.fa-cube.color-acento-contenido.mb-3(style="font-size: 28px;")
+              h4.mb-0.text-white #[em Sprint Planning Meeting]
+          .col-lg-4.col-md-6.mb-4
+            .fondo-color-sistema-k.borde-20.p-4.pb-5.text-center.h-100
+              i.fas.fa-cube.color-acento-contenido.mb-3(style="font-size: 28px;")
+              h4.mb-0.text-white #[em Daily Scrum]
+          .col-lg-4.col-md-6.mb-4
+            .fondo-color-primario.borde-20.p-4.pb-5.text-center.h-100
+              i.fas.fa-cube.color-acento-contenido.mb-3(style="font-size: 28px;")
+              h4.mb-0.text-white #[em Sprint Review]
+          .col-lg-4.col-md-6.mb-4
+            .fondo-color-sistema-k.borde-20.p-4.pb-5.text-center.h-100
+              i.fas.fa-cube.color-acento-contenido.mb-3(style="font-size: 28px;")
+              h4.mb-0.text-white #[em Sprint Retrospective]
+
+      .col-lg-6.order-first.order-lg-last.mb-4
+        .fondo-personalizado-5
+          figure.pb-4
+            img.mx-auto(src="@/assets/curso/temas/17.svg",style="max-width: 359px;")
+    
+    .row.pt-5.mb-5
+      .col-auto.d-none.d-md-block
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/18.svg",style="max-width: 85px;")
+      .col
+        p.mb-0 Consiste en realizar un análisis de los requerimientos del sistema (#[em Product Backlog]), señalar cuáles serán los objetivos a corto o mediano plazo dentro de un sprint, es decir, en la fase de desarrollo. Posteriormente, los desarrolladores realizan su trabajo, se efectúan algunas pruebas y se retroalimenta de acuerdo con lo conseguido al terminar la última fase.
+    
+    .titulo-sexto.color-acento-contenido
+      p.mb-0 #[b Figura 2.] Modelo ágil #[em Scrum]
+    .fondo-personalizado-4.pos-1.mb-5
+      .fondo-personalizado-4__contenido.pb-4.pt-5
+        figure
+          img.mx-auto(src="@/assets/curso/temas/19.svg",style="max-width: 1027px;",alt="La figura  presenta un resumen del marco de trabajo Agile Scrum, destacando roles clave como el Product Owner y el Scrum Master, y procesos como reuniones diarias de Scrum, planificación de sprints y retrospectivas. Se ilustra cómo las tareas se mueven desde el backlog del producto hasta su finalización a lo largo de un sprint que dura de 2 a 4 semanas.")
+
+    .row.mb-5
+      .col-auto.d-none.d-md-block
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/16.svg",style="max-width: 85px;")
+      .col
+        p.mb-0 A continuación, se explora el enfoque iterativo e incremental que Scrum ofrece para la gestión de proyectos de #[em software]:
+    
+    figure.mb-5(data-aos="zoom-in-up")
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/DSO1YzSAx4s?si=qHkm-SHyMtqXgQco" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
+      figcaption Video. Scrum y la especificación de requisitos
+    
+    .cont-titulo-decorativo.p-2.mt-5.mb-5
+      .row
+        .col-auto.pe-0
+          figure
+            img(src="@/assets/curso/temas/img-titulo-dec.svg")
+        .col.align-self-end
+          h3.mt-3.mb-0.me-2 #[em Test Driven Development (TDD)]
+    
+    .row.mb-4
+      .col-lg-7
+        p.mb-0 Según Astel (2003), el #[em Test Driven Development] es un enfoque de desarrollo que mantiene un exhaustivo conjunto de pruebas realizadas por el programador. Su objetivo principal es que ninguna parte del código avance a producción sin antes realizar las pruebas asociadas y obtener la aprobación correspondiente. Se trata de un desarrollo orientado a las pruebas, que cambia la mentalidad del equipo de desarrollo, agilizando los resultados y aumentando la calidad del producto. La tendencia actual es integrar TDD en cualquier metodología, ya sea ágil (Scrum Alliance - TDD and Scrum, 2011) o tradicional (Letelier et al., s. f.), para aprovechar los beneficios de practicar una metodología que permite corregir errores constantemente, asegurar la calidad del producto y protegerse de errores, tanto malintencionados como humanos.
+      .col-lg-5.order-first.order-lg-last
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/20.png",style="max-width: 505px;")
+    
+    .fondo-personalizado-4.pos-2
+      .fondo-personalizado-4__contenido.pb-5
+        .row.justify-content-center 
+          .col-lg-5
+            .titulo-sexto.color-acento-contenido.d-inline-block
+              p.mb-0 #[b Figura 3.] #[em Test Driven (TDD)]
+        figure
+          img.mx-auto(src="@/assets/curso/temas/21.svg",style="max-width: 820px;",alt="La figura presenta  un flujo de trabajo cíclico para el desarrollo de software, empezando por Inicio y terminando en Fin. El proceso incluye las etapas de Diseñar o mejorar pruebas, Ejecutar pruebas y Codificar/Desarrollar Software. Después de codificar, se realiza una Refactorización que puede llevar de nuevo al inicio del ciclo para una nueva iteración.")
+    
+    .cont-titulo-decorativo.p-2.mt-3.mb-5
+      .row
+        .col-auto.pe-0
+          figure
+            img(src="@/assets/curso/temas/img-titulo-dec.svg")
+        .col.align-self-end
+          h3.mt-3.mb-0.me-2 Mobile-D
+    .fondo-personalizado-6.mb-5
+      .fondo-personalizado-6__contenido
+        .row.mb-5
+          .col-lg-7
+            p.mb-0 Según Abrahamsson (2004) es una metodología de desarrollo diseñada específicamente para el desarrollo de aplicaciones móviles, que integra prácticas ágiles como #[em Extreme Programming y Crystal]. Presenta características tales como la orientación a pruebas, la programación en parejas, la integración continua y la refactorización, además de las tareas de mejora del proceso de #[em software]. La metodología incluye las fases de exploración, inicialización, producción, estabilización y pruebas. Cada fase cuenta con un día de planificación y un día de entrega.
+          .col-lg-5.order-first.order-lg-last
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/22.png",style="max-width: 505px;")
+
+
+        .row.justify-content-center 
+          .col-lg-10
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 4.] Ciclo de desarrollo #[em Mobile-D]
+            figure
+              img.mx-auto(src="@/assets/curso/temas/23.svg",style="max-width: 1029px;",alt="La figura despliega un diagrama de flujo para el desarrollo de software, organizado en etapas y fases desde la Exploración hasta las Pruebas del Sistema. Detalla actividades como configuración, planeación y liberación, repitiendo algunas actividades en varias fases para subrayar un proceso iterativo y progresivo.")
+
+    .row.mb-5
+      .col-auto.d-none.d-md-block
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/16.svg",style="max-width: 85px;")
+      .col
+        p.mb-0 A continuación, se explica cada fase:
+    
+    TabsB.color-acento-botones.mb-5(data-aos="flip-up")
+      .p-3.fondo-personalizado-7(titulo="<h4>Exploración</h4>" :icono="require('@/assets/curso/temas/25.svg')")
+        .row.justify-content-center.mb-5.mt-4
+          .col-lg-11
+            .row.align-items-center
+              .col-lg-7.mb-4.mb-lg-0
+                p.mb-4.fw-bold Exploración
+                p.mb-0 En esta fase se recolecta la información necesaria para la adecuada orientación del proyecto, sus requisitos, su alcance y las partes interesadas en el producto (#[em Stakeholders]).
+              .col-lg-5.order-last.order-lg-first
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/26.png",style="max-width: 400px;")
+      .p-3.fondo-personalizado-7(titulo="<h4>Inicialización</h4>" :icono="require('@/assets/curso/temas/25.svg')")
+        .row.justify-content-center.mb-5.mt-4
+          .col-lg-11
+            .row.align-items-center
+              .col-lg-7.mb-4.mb-lg-0
+                p.mb-4.fw-bold Inicialización
+                p.mb-0 Esta fase involucra la preparación de recursos físicos y tecnológicos, como la formación del equipo de trabajo y la disposición de los equipos tecnológicos de uso.
+              .col-lg-5.order-last.order-lg-first
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/27.png",style="max-width: 400px;")
+      .p-3.fondo-personalizado-7(titulo="<h4>Producción</h4>" :icono="require('@/assets/curso/temas/25.svg')")
+        .row.justify-content-center.mb-5.mt-4
+          .col-lg-11
+            .row.align-items-center
+              .col-lg-7.mb-4.mb-lg-0
+                p.mb-4.fw-bold Producción
+                p.mb-4 Implementación de las funcionalidades que requiere el producto mediante un desarrollo iterativo, dividido en tres etapas:
+                ul.lista-ul 
+                  li.mb-3
+                    span.fondo-color-acento-contenido.fw-bold.d-inline-flex.px-1.align-items-center.justify-content-center a)
+                    span Día de planificación, con ejecución de #[em tests] de aceptación.
+                  li.mb-3
+                    span.fondo-color-acento-contenido.fw-bold.d-inline-flex.px-1.align-items-center.justify-content-center b)
+                    span Día de trabajo, para el desarrollo de funcionalidades.
+                  li.mb-3
+                    span.fondo-color-acento-contenido.fw-bold.d-inline-flex.px-1.align-items-center.justify-content-center c)
+                    span Día de entrega de funcionalidad.
+              .col-lg-5.order-last.order-lg-first
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/28.png",style="max-width: 400px;")
+      .p-3.fondo-personalizado-7(titulo="<h4>Estabilización</h4>" :icono="require('@/assets/curso/temas/25.svg')")
+        .row.justify-content-center.mb-5.mt-4
+          .col-lg-11
+            .row
+              .col-lg-7.mb-4.mb-lg-0
+                p.mb-4.fw-bold Estabilización
+                p.mb-4 Fase de integración y verificación del correcto funcionamiento del sistema y la calidad del desarrollo. Incluye también la documentación del proyecto.
+              .col-lg-5.order-last.order-lg-first
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/29.png",style="max-width: 400px;")
+      .p-3.fondo-personalizado-7(titulo="<h4>Pruebas del sistema</h4>" :icono="require('@/assets/curso/temas/25.svg')")
+        .row.justify-content-center.mb-5.mt-4
+          .col-lg-11
+            .row
+              .col-lg-7.mb-4.mb-lg-0
+                p.mb-4.fw-bold Pruebas del sistema
+                p.mb-4 Verificación del funcionamiento de todos los requerimientos establecidos por el cliente y solución de errores encontrados.
+              .col-lg-5.order-last.order-lg-first
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/30.png",style="max-width: 400px;")
+    
+
 </template>
 
 <script>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,11 +1,139 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.pb-0.pb-md-0
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Plataformas de desarrollo móvil nativas
+    
+    p.mb-4 Las plataformas de desarrollo móvil nativas, son aquellas que tienen un desarrollo específico para cada uno de sus sistemas operativos; #[em IOS, Android, o Windows Phone], lo que quiere decir que para cada uno de estos Sistemas Operativos se adapta un lenguaje de desarrollo así:
+
+    .fondo-personalizado-4.pos-1.mb-5
+      .fondo-personalizado-4__contenido.pt-4
+        .row.justify-content-center
+          .col-lg-4.mb-5(data-aos="fade-right")
+            .tarjeta-img-1
+              figure.tarjeta-img-1__img
+                img.mx-auto(src="@/assets/curso/temas/32.png")
+              .tarjeta-img-1__texto.p-3.fondo-color-primario
+                p Sistema Operativo Móvil: iOS, Lenguajes: Objetive-C, Swift
+          .col-lg-4.mb-5(data-aos="fade-up")
+            .tarjeta-img-1
+              figure.tarjeta-img-1__img
+                img.mx-auto(src="@/assets/curso/temas/33.png")
+              .tarjeta-img-1__texto.p-3.fondo-color-primario
+                p Sistema Operativo Móvil: Android, Lenguajes: Java, Kotlin
+
+          .col-lg-4.mb-5(data-aos="fade-left")
+            .tarjeta-img-1
+              figure.tarjeta-img-1__img
+                img.mx-auto(src="@/assets/curso/temas/34.png")
+              .tarjeta-img-1__texto.p-3.fondo-color-primario
+                p.mb-0  Sistema Operativo Móvil: Windows Phone
+                p Lenguajes: C++, C#
+
+    .row
+      .col-lg-8.mb-4.mb-lg-0
+        p.mb-0 Una de las principales ventajas de las aplicaciones nativas es que pueden aprovechar todas las funcionalidades avanzadas de los dispositivos de cada plataforma, incluidos los procesadores gráficos. Los mayores beneficios de este tipo de aplicaciones siempre serán la flexibilidad y la usabilidad, que son superiores a las de las aplicaciones híbridas. Uno de los principales inconvenientes del desarrollo de las aplicaciones nativas es que tanto el desarrollo como las actualizaciones de estas apps tienen un alto costo.
+      .col-lg-4
+        figure
+          img.mx-auto(src="@/assets/curso/temas/35.svg",style="max-width: 400px;")
+    
+    .cont-titulo-decorativo.p-2.mb-5.mt-lg-2.mt-5
+      .row
+        .col-auto.pe-0
+          figure
+            img(src="@/assets/curso/temas/img-titulo-dec.svg")
+        .col.align-self-end
+          h3.mt-3.mb-0.me-2 Rendimiento
+    
+    p.mb-4 El rendimiento de las aplicaciones nativas es otra de sus características principales, ya que ofrecen mayor velocidad, animaciones avanzadas, transiciones complejas, aprovechamiento total del hardware (GPS, cámara, sensores, etc.) y un menor consumo de memoria.
+    p Ventajas de las aplicaciones nativas:
+
+    .fondo-personalizado-6.pos-1.mb-5
+      .fondo-personalizado-6__contenido.pt-5
+        .row.justify-content-center 
+          .col-lg-8
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #4F4415;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Permiten el desarrollo para diferentes tipos de dispositivos como #[em wearables], TV o carros.
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #892758;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Ofrecen un nivel de seguridad superior al de las aplicaciones híbridas.
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #5F2789;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Tienen un mayor grado de optimización.
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #760ED3;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Acceso a funcionalidades de accesibilidad nativas.
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #15464F;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Proporcionan un entorno que permite el uso de herramientas de arrastrar y soltar para el diseño de interfaces de usuario.
+            .py-4.py-md-1.px-4.borde-29(style="background-color: #78401F;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Disponen de SDKs para la optimización de librerías.
+    
+        .cont-titulo-decorativo.p-2.mt-5
+          .row
+            .col-auto.pe-0
+              figure
+                img(src="@/assets/curso/temas/img-titulo-dec.svg")
+            .col.align-self-end
+              h3.mt-3.mb-0.me-2 Arquitectura de las aplicaciones móviles
+    
+    p.mb-5 Básicamente, el desarrollo de las #[em apps] móviles se compone de dos partes, las cuales se diferencian por su código: el #[em front-end] y la parte de web #[em services].
+
+    .row.justify-content-center.mb-5
+      .col-lg-5.mb-4(data-aos="fade-right")
+        .tarjeta-personalizada-1.p-5.h-100.pb-4
+          .burbuja-1.fondo-color-acento-contenido.me-auto.mb-5
+            figure.p-1
+              img.mx-auto(src="@/assets/curso/temas/37.svg",style="max-width: 57px;")
+          p.mb-5.text-white El #[em front-end] también llamado parte cliente, es el segmento de lógica de visualización e interacción con el usuario, que se ejecuta en los dispositivos a través de su sistema operativo, como Android o iOS.
+      .col-lg-5.mb-4(data-aos="fade-left")
+        .tarjeta-personalizada-1.p-5.h-100.pb-4
+          .burbuja-1.fondo-color-acento-contenido.me-auto.mb-5
+            figure.p-1
+              img.mx-auto(src="@/assets/curso/temas/38.svg",style="max-width: 58px;")
+          p.mb-5.text-white La parte de #[em web services], o parte servidora, es donde se encuentra la lógica del negocio de las aplicaciones, la persistencia de datos y la interacción con otras plataformas.
+    
+    .row.justify-content-center
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido.mb-2
+          p.mb-0 #[b Figura 5.] Arquitectura de una #[em app]
+    .fondo-personalizado-3.pos-1
+      .fondo-personalizado-3__contenido.pb-5.pt-4
+        .row.justify-content-center.pb-5
+          .col-lg-8.pb-3
+            figure
+              img.mx-auto(src="@/assets/curso/temas/40.svg",style="max-width: 820px;",alt="La figura  presenta un esquema de arquitectura de red donde diferentes tipos de clientes, como Android, iOS, Windows Mobile, Blackberry y Windows Desktop, se conectan a través de HTTP/S a un servidor denominado Magic Server. Este servidor gestiona metadatos de la aplicación y datos de la aplicación, facilitando la comunicación y el intercambio de información entre los clientes y el servidor.")
+
+
 
 </template>
 
