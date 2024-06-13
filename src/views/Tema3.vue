3735365fc559d7e66bd8de9c539278b2798792a3
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-06-12 22:23:51 -0500

    avances

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.pb-0.pb-md-0
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
@@ -180,7 +180,230 @@
             img(src="@/assets/curso/temas/img-titulo-dec.svg")
         .col.align-self-end
           h3.mt-3.mb-0.me-2 Arquitectura
-         
+    
+    .row
+      .col-auto.d-none.d-md-block
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/16.svg",style="max-width: 85px;")
+      .col
+        p.mb-0 Los componentes principales de la plataforma Android están diseñados para una variedad de dispositivos. Android es de código abierto y está basado en Linux.
+    
+    p(style="color: red;") faltantes !!!!
+
+    .row.mb-4
+      .col-auto.d-none.d-md-block
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/16.svg",style="max-width: 85px;")
+      .col
+        p.mb-0 A continuación, se presenta una descripción completa del IDE Android Studio, que como se mencionó anteriormente, es el entorno oficial para el desarrollo de aplicaciones móviles nativas en Android.
+    
+    Separador
+
+    #t_3_2.titulo-segundo.color-acento-contenido
+      h2 3.2 Android Studio
+    .fondo-personalizado-6.pos-2
+      .fondo-personalizado-6__contenido
+        .row.mb-5
+          .col-lg-7
+            p.mb-4 Android Studio está basado en IntelliJ IDEA de la compañía JetBrains, que proporciona varias mejoras con respecto al plugin ADT (Android Developer Tools) para Eclipse. Android Studio utiliza una licencia de #[em software] libre Apache 2.0, está programado en Java y es multiplataforma. Fue presentado por Google el 16 de mayo de 2013 en el congreso de desarrolladores Google I/O, con el objetivo de crear un entorno dedicado exclusivamente a la programación de aplicaciones para dispositivos Android, proporcionando a Google un mayor control sobre el proceso de producción. Actualmente, es el IDE recomendado por Google.
+            p.mb-0 Las características son:
+          .col-lg-5.order-first.order-lg-last
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/49.png",style="max-width: 502px;")
+
+        .row.justify-content-center.pt-3
+          .col-lg-8
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #4F4415;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Realiza renderizados de layouts en tiempo real, ejecutando las compilaciones de forma rápida.
+        .row 
+          .col-lg-2.me-3
+          .col-lg-8
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #892758;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Permite ejecutar la aplicación en tiempo real en el dispositivo móvil.
+
+        .row 
+          .col-lg-2.me-5
+          .col-lg-8
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #5F2789;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Incluye un potente emulador.
+
+        .row 
+          .col-lg-2.me-5
+          .col-lg-8.ms-lg-4
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #760ED3;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Permite simular diferentes dispositivos y tabletas.
+
+        .row.mb-5
+          .col-lg-3
+          .col-lg-8
+            .py-4.py-md-1.px-4.borde-29.mb-2(style="background-color: #15464F;")
+              .row.align-items-center
+                .col-md-auto 
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 50px;")
+                .col 
+                  p.mb-0.text-white.fw-bold Trabaja con un editor de elementos gráficos para la creación de la interfaz sin necesidad de código, solo arrastrando y soltando.
+
+        .row
+          .col-auto.d-none.d-md-block
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/16.svg",style="max-width: 85px;")
+          .col
+            p.mb-0 La realización de aplicaciones en Android Studio se enfoca en 4 fases de desarrollo:
+
+    .row.mt-5.pt-2.mb-3
+      .col-lg-3.mb-4
+        .tarjeta-personalizada-2.pb-4.h-100
+          .d-flex.mb-5
+            .burbuja-1.fondo-color-acento-contenido.mx-auto
+              figure.p-3
+                img.mx-auto(src="@/assets/curso/temas/50.svg",style="max-width: 70px;")
+          p.mb-0.c-color-acento-contenido.fw-bold Fase 1.
+          p.mb-4.c-color-acento-contenido.fw-bold Configuración de entorno.
+          p.mb-0.text-white En esta fase se realiza la instalación y configuración del entorno de desarrollo, se conectan dispositivos y se crean emuladores virtuales.
+      .col-lg-3.mb-4
+        .tarjeta-personalizada-2.pb-4.h-100
+          .d-flex.mb-5
+            .burbuja-1.fondo-color-acento-contenido.mx-auto
+              figure.p-3
+                img.mx-auto(src="@/assets/curso/temas/51.svg",style="max-width: 69px;")
+          p.mb-0.c-color-acento-contenido.fw-bold Fase 2.
+          p.mb-4.c-color-acento-contenido.fw-bold Configuración del Proyecto y Desarrollo.
+          p.mb-0.text-white Esta fase involucra configurar los módulos necesarios que contienen recursos de la aplicación y los archivos de código fuente.
+      .col-lg-3.mb-4
+        .tarjeta-personalizada-2.pb-4.h-100
+          .d-flex.mb-5
+            .burbuja-1.fondo-color-acento-contenido.mx-auto
+              figure.p-3
+                img.mx-auto(src="@/assets/curso/temas/52.svg",style="max-width: 70px;")
+          p.mb-0.c-color-acento-contenido.fw-bold Fase 3.
+          p.mb-4.c-color-acento-contenido.fw-bold Pruebas, depuración y Construcción de la aplicación.
+          p.mb-0.text-white Se crea el ejecutable o APK que puede ejecutarse e instalarse en el emulador o dispositivo Android.
+      .col-lg-3.mb-4
+        .tarjeta-personalizada-2.pb-4.h-100
+          .d-flex.mb-5
+            .burbuja-1.fondo-color-acento-contenido.mx-auto
+              figure.p-3
+                img.mx-auto(src="@/assets/curso/temas/53.svg",style="max-width: 70px;")
+          p.mb-0.c-color-acento-contenido.fw-bold Fase 4.
+          p.mb-4.c-color-acento-contenido.fw-bold Publicación de la aplicación.
+          p.mb-0.text-white En esta fase se realiza la solicitud en la tienda PlayStore para la distribución libre de la aplicación, permitiendo que los usuarios puedan descargarla e instalarla en sus dispositivos Android.
+    
+    Separador
+
+    #t_3_3.titulo-segundo.color-acento-contenido
+      h2 3.3 Arquitectura de una aplicación en Android
+    
+    .row.mb-5
+      .col-lg-7
+        p.mb-4 Cada componente depende solo del componente que está un nivel más abajo. Por ejemplo, las actividades y los fragmentos solo dependen de un modelo de vista. El repositorio es la única clase que depende de otras clases. En este ejemplo, el repositorio depende de un modelo de datos persistente y de una fuente de datos de #[em back-end] remota.
+        .fondo-color-sistema-m.p-4.borde-10
+          .row.align-items-center 
+            .col-md-auto 
+              figure.mb-4.mb-md-0
+                img.mx-auto(src="@/assets/curso/temas/54.svg",style="max-width: 100px;")
+            .col 
+              p.mb-0 Este diseño crea una experiencia de usuario consistente y agradable. Independientemente de que el usuario vuelva a la app varios minutos después de cerrarla por última vez o varios días más tarde, verá instantáneamente la información del usuario que la app persiste a nivel local. Si estos datos están inactivos, el módulo de repositorio comienza a actualizar los datos en segundo plano.
+      .col-lg-5.order-first.order-lg-last
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/55.png",style="max-width: 502px;")
+
+    .fondo-personalizado-burbuja
+      .fig-burbuja.left(style="bottom: 0;")
+      .fig-burbuja.right
+      .fondo-personalizado-burbuja__contenido
+        .row.justify-content-center.mb-5
+          .col-lg-6
+            .titulo-sexto.color-acento-contenido.mb-5
+              p.mb-0 #[b Figura 7.] Arquitectura de una aplicación en Android
+            
+            figure
+              img.mx-auto(src="@/assets/curso/temas/56.svg",style="max-width: 610px;",alt="La figura muestra un diagrama de arquitectura de aplicación, donde una Actividad o Fragmento se conecta a un ViewModel que utiliza LiveData. Un Repository central gestiona la comunicación entre ViewModel y las fuentes de datos, divididas en Model con Room para la base de datos local y un Remote Data Source usando Retrofit para la conexión a servicios externos.")
+    
+    h4.fondo-color-primario.mb-5.text-white.px-4.py-2.d-inline-block.borde-20 Versiones
+
+    .row.mb-5
+      .col-auto.d-none.d-md-block
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/16.svg",style="max-width: 85px;")
+      .col
+        p.mb-0 Así como el sistema operativo Android tiene una gran variedad de actualizaciones, el ritmo de actualizaciones de Android Studio es bastante alto. A continuación, se listan las últimas versiones desde la más actual hasta la más antigua.
+
+    .fondo-personalizado-4
+      .fondo-personalizado-4__contenido.pb-5
+        .row.justify-content-center.pb-5
+          .col-lg-8
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Tabla 2.] Nombres versiones y lanzamiento de sistema operativo Android
+
+            .tabla-a.color-acento-contenido.fondo-color-sistema-h
+              table
+                thead
+                  tr
+                    th Versiones Android Studio 
+                    th Fecha de lanzamiento
+                tbody
+                  tr
+                    td Android Studio Bumblebee (2021.1.1)
+                    td 27 de junio de 2021
+                  tr
+                    td Android Studio Arctic Fox (2020.3.1) Beta 3
+                    td 27 de mayo de 2021
+                  tr
+                    td Android Studio 4.2.1
+                    td 13 de mayo de 2021
+                  tr
+                    td Android Studio 4.2.0
+                    td 4 de mayo de 2021
+                  tr
+                    td Android Studio 4.1.3
+                    td 18 de marzo de 2021
+                  tr
+                    td Android Studio 4.1
+                    td 12 de octubre de 2020
+                  tr
+                    td Android Studio 4.0
+                    td 28 de mayo de 2020
+                  tr
+                    td Android Studio 3.6.3
+                    td 17 de abril de 2020
+                  tr
+                    td Android Studio 3.0
+                    td 25 de octubre de 2017
+                  tr
+                    td Android Studio 2.3.2
+                    td 11 de mayo de 2017
+                  tr
+                    td Android Studio Chipmunk (2021.2.1)
+                    td 9 de mayo de 2022
+                  tr
+                    td Android Studio Dolphin (2021.3.1)
+                    td 8 de agosto de 2022
+                  tr
+                    td Android Studio Electric Eel (2022.1.1)
+                    td 27 de marzo de 2023
+                 
+
 
 </template>
 
